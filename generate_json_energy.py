472f96dfff929e3be0467dc187a3ec273fd97266
M.tatsuya <matsuu2tatsuya@gmail.com>
2021-06-23 23:09:53 +0900

    third commit

--- generate_json_energy.py
+++ generate_json_energy.py
@@ -3,7 +3,7 @@ import numpy as np
 
 if __name__ == '__main__':
     json_data = {
-        "type": "walking",
+        "type": "energy",
         "result": []
     }
     for i in range(7, 26):
@@ -12,36 +12,32 @@ if __name__ == '__main__':
         w_count = np.random.normal(
             loc=2700,  # 平均
             scale=1000,  # 標準偏差
-            size=1,  # 出力配列のサイズ(タプルも可)
+            size=2,  # 出力配列のサイズ(タプルも可)
         )
         count = round(int(w_count[0]))
         if 3000 > count > 2500:
             feeling_type = "perfect"
-            distance_meter = round(count * 0.7)
-            time_hour = round((count * 0.55) / 3600, 1)
-            speed_kilometer = round(distance_meter / time_hour / 1000, 1)
+            burn_energy = round(int(w_count[1]))
+            energy_balance = count - burn_energy
         elif 3500 > count > 1800:
             feeling_type = "happy"
-            distance_meter = round(count * 0.7)
-            time_hour = round((count * 0.6) / 3600, 1)
-            speed_kilometer = round(distance_meter / time_hour / 1000, 1)
+            burn_energy = round(int(w_count[1]))
+            energy_balance = count - burn_energy
         elif 4200 > count > 1500:
             feeling_type = "lazy"
-            distance_meter = round(count * 0.7)
-            time_hour = round((count * 0.65) / 3600, 1)
-            speed_kilometer = round(distance_meter / time_hour / 1000, 1)
+            burn_energy = round(int(w_count[1]))
+            energy_balance = count - burn_energy
         else:
             feeling_type = "slump"
-            distance_meter = round(count * 0.7)
-            time_hour = round((count * 0.7) / 3600, 1)
-            speed_kilometer = round(distance_meter / time_hour / 1000, 1)
+            burn_energy = round(int(w_count[1]))
+            energy_balance = count - burn_energy
         data = {
             "date": f"2021年02月{str(i)}日00時00分",
             "feeling": feeling_type,
             "count": count,
-            "distance_meter": distance_meter,
-            "time_hour": time_hour,
-            "speed_kilometer": speed_kilometer
+            "intake_energy": count,
+            "energy_balance": energy_balance,
+            "burn_energy": burn_energy
         }
         json_data['result'].append(data)
     f = open(f'wepo_energy_test.json', 'w')
---
+++ generate_json_sleep_base_temp.py
@@ -0,0 +1,42 @@
+import json
+import numpy as np
+
+if __name__ == '__main__':
+    json_data = {
+        "type": "sleep_temp",
+        "result": []
+    }
+    for i in range(7, 26):
+        if i < 9:
+            i = f"0{str(i)}"
+        w_count = np.random.normal(
+            loc=6,  # 平均
+            scale=2,  # 標準偏差
+            size=1,  # 出力配列のサイズ(タプルも可)
+        )
+        count = round(float(w_count[0]), 1)
+        if 8 > count > 6.5:
+            feeling_type = "perfect"
+            base_temp = round(float(np.random.normal(loc=36.7, scale=0.3, size=1)[0]), 1)
+        elif 10 > count > 5.5:
+            feeling_type = "happy"
+            base_temp = round(float(np.random.normal(loc=36.7, scale=0.3, size=1)[0]), 1)
+        elif 12 > count > 5:
+            feeling_type = "lazy"
+            base_temp = round(float(np.random.normal(loc=36.7, scale=0.3, size=1)[0]), 1)
+        else:
+            feeling_type = "slump"
+            base_temp = round(float(np.random.normal(loc=36.7, scale=0.3, size=1)[0]), 1)
+        if base_temp > 37.0:
+            feeling_type = "lazy"
+        elif base_temp > 37.5:
+            feeling_type = "slump"
+        data = {
+            "date": f"2021年02月{str(i)}日00時00分",
+            "feeling": feeling_type,
+            "sleep_time": count,
+            "base_temperature": base_temp,
+        }
+        json_data['result'].append(data)
+    f = open(f'wepo_sleep_baseTemp_test.json', 'w')
+    json.dump(json_data, f, ensure_ascii=False, indent=4, separators=(',', ':'))
